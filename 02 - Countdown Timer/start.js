c86f92398104264a0a96254d014eeafa72c2197d
kritsana.p1 <kritsana.p@merudy.com>
2022-03-14 17:30:22 +0700

    Day 2 : Countdown Timmer 2022

--- 02 - Countdown Timer/start.js
+++ 02 - Countdown Timer/start.js
@@ -1,4 +1,41 @@
 (() => {
-  // เริ่มเขียนโค้ด
+    // เริ่มเขียนโค้ด
+    const SECOND = 1000;
+    const MINUTE = SECOND * 60;
+    const HOUR = MINUTE * 60;
+    const DAY = HOUR * 24;
 
+    function setElementInnerTextHTML(id, data) {
+        const element = document.getElementById(id);
+        element.innerText = data;
+    }
+
+    function run() {
+        const now = new Date().getTime();
+        const newYear = new Date("December 31, 2022 00:00:00").getTime();
+        const unixTimeLeft = newYear - now;
+
+        setElementInnerTextHTML("days", Math.floor(unixTimeLeft / DAY));
+        setElementInnerTextHTML("hours", Math.floor((unixTimeLeft % DAY) / HOUR));
+        setElementInnerTextHTML(
+            "minutes",
+            Math.floor((unixTimeLeft % HOUR) / MINUTE)
+        );
+        setElementInnerTextHTML(
+            "seconds",
+            Math.floor((unixTimeLeft % MINUTE) / SECOND)
+        );
+
+        console.log(`now : ${now}`);
+        console.log(`newYear : ${newYear}`);
+        console.log(`unixTimeLeft : ${unixTimeLeft}`);
+        console.log(`DAY : ${unixTimeLeft / DAY}`);
+        console.log(`HOUR : ${(unixTimeLeft % DAY) / HOUR}`);
+        console.log(`MINUTES : ${(unixTimeLeft % HOUR) / MINUTE}`);
+        console.log(`SECOND : ${(unixTimeLeft % MINUTE) / SECOND}`);
+
+        setInterval(run, SECOND);
+    }
+
+    run();
 })();
